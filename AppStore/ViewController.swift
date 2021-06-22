a3fdc9bc12e7306082c5d2d0c68e3dd981465d0f
gyun <gyun@mobmail.shinhan.com>
2021-06-22 16:26:05 +0900

    init project

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Chang gyun Park on 2021/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
